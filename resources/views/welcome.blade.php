1747ad0e9cf437d3e8809988d1d8853c6264367e
José Gallardo <ing_gallardo@outlook.com>
2020-01-13 13:19:50 -0600

    Sponsors update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1532,49 +1532,55 @@
                 <div class="row no-gutters supporters-wrap clearfix">
                     <div class="col-lg-3 col-md-4 col-xs-6">
                         <div class="supporter-logo">
-                            <img src="img/supporters/1.png" class="img-fluid" alt="" />
+                            <img src="img/supporters/chevrolet.jpg" class="img-fluid" alt="" />
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-xs-6">
                         <div class="supporter-logo">
-                            <img src="img/supporters/2.png" class="img-fluid" alt="" />
+                            <img src="img/supporters/karla.png" class="img-fluid" alt="" />
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-xs-6">
                         <div class="supporter-logo">
-                            <img src="img/supporters/3.png" class="img-fluid" alt="" />
+                            <img src="img/supporters/jumex.png" class="img-fluid" alt="" />
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-xs-6">
                         <div class="supporter-logo">
-                            <img src="img/supporters/4.png" class="img-fluid" alt="" />
+                            <img src="img/supporters/best_western.jpg" class="img-fluid" alt="" />
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-xs-6">
                         <div class="supporter-logo">
-                            <img src="img/supporters/5.png" class="img-fluid" alt="" />
+                            <img src="img/supporters/jardin_cardenales.jpg" class="img-fluid" alt="" />
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-xs-6">
                         <div class="supporter-logo">
-                            <img src="img/supporters/6.png" class="img-fluid" alt="" />
+                            <img src="img/supporters/logo_molinillos.jpg" class="img-fluid" alt="" />
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-xs-6">
                         <div class="supporter-logo">
-                            <img src="img/supporters/7.png" class="img-fluid" alt="" />
+                            <img src="img/supporters/notaria_1.jpg" class="img-fluid" alt="" />
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-xs-6">
                         <div class="supporter-logo">
-                            <img src="img/supporters/8.png" class="img-fluid" alt="" />
+                            <img src="img/supporters/logotipo-gas-imperial.jpeg" class="img-fluid" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-xs-6">
+                        <div class="supporter-logo">
+                            <img src="img/supporters/panque.jpeg" class="img-fluid" alt="" />
                         </div>
                     </div>
                 </div>
